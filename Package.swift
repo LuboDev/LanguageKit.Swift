3812d1bb4ab72c407d15d76d98391b00d89029cb
Ermat <ealymbaev@gmail.com>
2022-10-06 17:11:31 +0600

    Organize and update source code

--- Package.swift
+++ Package.swift
@@ -3,21 +3,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "LanguageKit",
-    platforms: [
-        .iOS(.v13),
-    ],
-    products: [
-        .library(
-            name: "LanguageKit",
-            targets: ["LanguageKit"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/horizontalsystems/UIExtensions.git", .upToNextMajor(from: "1.1.1")),
-    ],
-    targets: [
-        .target(
-            name: "LanguageKit",
-            dependencies: ["UIExtensions"]),
-    ]
+        name: "LanguageKit",
+        platforms: [
+            .iOS(.v13),
+        ],
+        products: [
+            .library(
+                    name: "LanguageKit",
+                    targets: ["LanguageKit"]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
+        ],
+        targets: [
+            .target(
+                    name: "LanguageKit",
+                    dependencies: [
+                        .product(name: "UIExtensions", package: "UIExtensions.Swift"),
+                    ]
+            ),
+        ]
 )
